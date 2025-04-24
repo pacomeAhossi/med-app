3624efd7ec4a58a4711a7dd664cee6ebcca4c695
Pacome AHOSSI <pacomeahossi175@gmail.com>
2025-04-24 08:23:09 -0400

    Creation du composant DoctorCard

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import Landing_Page from "./Components/Landing_Page/Landing_Page";
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
 import Login from "./Components/Login/Login";
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import DoctorCard from "./Components/DoctorCard/DoctorCard";
 
 function App() {
   return (
     <div className="App">
         <Router>
-            <Navbar/>
+            {/* <Navbar/> */}
             <Routes>
                 <Route path="/" element={<Landing_Page />} />
                 <Route path="/signup" element={<Sign_Up />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/instant-consultation" element={<InstantConsultation />} />
+                <Route path="/doctor-card" element={<DoctorCard />} />
             </Routes>
         </Router>
     </div>
